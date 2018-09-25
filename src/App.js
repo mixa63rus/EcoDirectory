0651ca40f13805158f176359f05e78dbc52c4dc9
Mixa63rus <mixa63rus@mail.ru>
2018-09-25 21:01:54 +0400

    Add "select"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Select from 'react-select';
 import './App.css';
+import {options} from './elements';
+
+const customStyles = {
+  option: (base, { isDisabled, isSelected }) => ({
+    ...base,
+    // borderBottom: '1px dotted pink',
+    padding: 10,
+    width: "500px",
+    backgroundColor: isDisabled
+        ? null
+        : isSelected ? "#DCDCDC" : null,
+      color: isDisabled
+        ? '#ccc'
+        : isSelected
+          ? "blue"
+          : "black",
+      cursor: isDisabled ? 'not-allowed' : 'default',
+  }),
+  menu: (styles) => ({ ...styles, width: "500px"}),
+  control: styles => ({ ...styles, backgroundColor: 'white', width: "500px" }),
+  singleValue: (base, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+    const transition = 'opacity 300ms';
+
+    return { ...base, opacity, transition };
+  }
+}
 
 class App extends Component {
+  state = {
+    selectedOption: null,
+  }
+
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption);
+  }
+  
   render() {
+    const { selectedOption } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <Select
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={options}
+        styles={customStyles}
+      />
+      {this.state.selectedOption ? <h1>{this.state.selectedOption.value}</h1> : null}
       </div>
     );
   }
