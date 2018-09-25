24e9fe0726db7db7ad46f2e3f76a4a7c0a1fed02
Mixa63rus <mixa63rus@mail.ru>
2018-09-25 23:58:17 +0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
-import './App.css';
 import {options} from './elements';
+import CustomNavbar from './CustomNavbar';
+import './App.css';
 
 const customStyles = {
   option: (base, { isDisabled, isSelected }) => ({
     ...base,
-    // borderBottom: '1px dotted pink',
     padding: 10,
-    width: "500px",
-    backgroundColor: isDisabled
-        ? null
-        : isSelected ? "#DCDCDC" : null,
-      color: isDisabled
-        ? '#ccc'
-        : isSelected
-          ? "blue"
-          : "black",
-      cursor: isDisabled ? 'not-allowed' : 'default',
+    width: "300px",
+    backgroundColor: isDisabled ? null
+      : isSelected ? "#DCDCDC" : null,
+    color: isDisabled ? '#ccc'
+      : isSelected ? "blue"
+      : "black",
+    cursor: isDisabled ? 'not-allowed' : 'default',
   }),
-  menu: (styles) => ({ ...styles, width: "500px"}),
-  control: styles => ({ ...styles, backgroundColor: 'white', width: "500px" }),
+  menu: (styles) => ({ ...styles, width: "300px", marginLeft: 30}),
+  control: styles => ({ ...styles, backgroundColor: 'white', width: "300px", margin: 30 }),
   singleValue: (base, state) => {
     const opacity = state.isDisabled ? 0.5 : 1;
     const transition = 'opacity 300ms';
@@ -43,14 +40,17 @@ class App extends Component {
     const { selectedOption } = this.state;
 
     return (
-      <div>
-      <Select
-        value={selectedOption}
-        onChange={this.handleChange}
-        options={options}
-        styles={customStyles}
-      />
-      {this.state.selectedOption ? <h1>{this.state.selectedOption.value}</h1> : null}
+      <div className="App">
+        <div>
+          <CustomNavbar />
+          <Select
+            value={selectedOption}
+            onChange={this.handleChange}
+            options={options}
+            styles={customStyles}
+          />
+          {this.state.selectedOption ? <h1>{this.state.selectedOption.value}</h1> : null}
+        </div>
       </div>
     );
   }
---
+++ src/CustomNavbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
+class CustomNavbar extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#home" style={{ fontFamily: "Great Vibes", fontSize: 35, color: "green"}}><b>EcoDirectory</b></a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="#">
+                            Database
+                        </NavItem>
+                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar>
+            </div>
+        )
+    }
+}
+
+export default CustomNavbar;
